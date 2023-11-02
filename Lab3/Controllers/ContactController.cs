d2d8dc6373f2b87608277e638a50b677a4518c7c
Moneybolt <szymgithub@gmail.com>
2023-11-02 16:03:33 +0100

    Add project files.

---
+++ Lab3/Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        public IActionResult Index()
+        {
+            return View(_contactService.FindAll());
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contactService.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Update(int id) 
+        {
+            return View(_contactService.FindById(id));
+        }
+        [HttpPost]
+        public IActionResult Update(Contact model) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _contactService.Update(model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Lab3/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Musisz podać imię")]
+        [StringLength(maximumLength: 50, ErrorMessage = "zbyt długo")]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [DataType(DataType.Date)]
+        public string? Birth { get; set; }
+    }
+}
